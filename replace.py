cd15b3b6c9156c570d50faa69303648e7bd3c161
Ayaya <3329058005@qq.com>
2024-09-20 16:34:49 +0800

    save

--- replace.py
+++ replace.py
@@ -6,7 +6,6 @@ paths = ["./templates", "./static/js"]
 
 with open("./static/locales/zh-CN.json", "r") as r:
     dic = json.load(r)
-    print(dic)
 
 
 def get_first_key(val):
@@ -16,18 +15,15 @@ def get_first_key(val):
     return None
 
 
-def get_character():
-    for path in paths:
-        for file in Path(path).glob("*"):
-            with open(file, "r") as f:
-                content = f.read()
-                for quote, text, _ in re.findall(
-                    r"([`'\"])(.*?)(\1)",
-                    f.read(),
-                ):
-                    if text in dic.values():
-                        content = content.replace(text, get_first_key(text))
-            for k, v in dic:
-                content = content.replace(v, k)
-            with open(file, "w") as f:
-                f.write(content)
+for path in paths:
+    for file in Path(path).glob("*"):
+        with open(file, "r") as f:
+            content = f.read()
+            for _, text, _ in re.findall(
+                r"([`'\"])(.*?)(\1)",
+                f.read(),
+            ):
+                if text in dic.values():
+                    content = content.replace(text, get_first_key(text))
+        with open(file, "w") as f:
+            f.write(content)
